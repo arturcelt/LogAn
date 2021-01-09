44d8e29034b61bdb09e0f591c20bc097965ff983
Artur Dybysławski <adybyslawski@hotmail.com>
2021-01-09 13:05:58 +0100

    REV_L_2.2

--- LogAnalyzerTests.cs
+++ LogAnalyzerTests.cs
@@ -8,6 +8,31 @@ namespace LogAn.UnitTests
     [TestFixture]
     public class LogAnalyzerTests
     {
+        private LogAnalyzer m_Analyzer = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_Analyzer = new LogAnalyzer();
+        }
+
+
+        [Test]
+        public void IsValidLogFileName_ValidFileLowerCased_ReturnsTrue()
+        {
+            bool result = m_Analyzer.IsValidLogFileName("whatever.slf");
+
+            Assert.IsTrue(result, "Plik powinien mieć prawidłową nazwę!");
+        }
+
+        [Test]
+        public void IsValidLogFileName_ValidFileUpperCased_ReturnsTrue()
+        {
+            bool result = m_Analyzer.IsValidLogFileName("whatever.SLF");
+
+            Assert.IsTrue(result);
+        }
+
         [Test]
         public void IsValidLogFileName_BadExtension_ReturnsFalse()
         {
@@ -18,17 +43,13 @@ namespace LogAn.UnitTests
             Assert.False(result);
         }
 
-  
-        [TestCase("filewithgoodextension.slf", true)]
-        [TestCase("filewithgoodextension.SLF", true)]
-        [TestCase("plikozlymrozszerzeniu.foo", false)]
-        public void IsValidLogFileName_VariousExtensions_ChecksThem(string fileName, bool expected)
+        [TearDown]
+        public void TearDown()
         {
-            LogAnalyzer analyzer = new LogAnalyzer();
-
-            bool result = analyzer.IsValidLogFileName(fileName);
-
-            Assert.AreEqual(result, expected);
+            m_Analyzer = null;
         }
+
+  
+      
     }
 }
