8e17c5877813af71d821089be7a46896e24690c4
Artur Dybysławski <adybyslawski@hotmail.com>
2021-01-09 12:54:23 +0100

    REV_INIT

---
+++ LogAnalyzerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("plikozlymrozszerzeniu.foo");
+
+            Assert.False(result);
+        }
+
+  
+        [TestCase("filewithgoodextension.slf", true)]
+        [TestCase("filewithgoodextension.SLF", true)]
+        [TestCase("plikozlymrozszerzeniu.foo", false)]
+        public void IsValidLogFileName_VariousExtensions_ChecksThem(string fileName, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(fileName);
+
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
