4b128d5c147ef814a5ff649180bffb04e29f9fed
Artur Dybysławski <adybyslawski@hotmail.com>
2021-01-24 14:56:38 +0100

    REV_5.2

---
+++ FakeLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogAn.UnitTests
+{
+    class FakeLogger : ILogger
+    {
+        public string LastError;
+
+        public void LogError(string message)
+        {
+            LastError = message;
+        }
+    }
+}
---
+++ LogAnalyzerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    class LogAnalyzerTests
+    {
+        [Test]
+        public void Analyze_ToShortFileName_CallLogger()
+        {
+            FakeLogger logger = new FakeLogger();
+
+            LogAnalyzer analyzer = new LogAnalyzer(logger);
+
+            analyzer.MinNameLength = 6;
+            analyzer.Analyze("a.txt");
+
+            StringAssert.Contains("za krótka", logger.LastError);
+        }
+    }
+}
