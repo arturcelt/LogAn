52ac6292da1735847f87da7284e3778c8aac9b17
Artur Dybysławski <adybyslawski@hotmail.com>
2021-01-25 19:08:37 +0100

    REV_5.4

---
+++ ExampleTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class ExampleTests
+    {
+        [Test]
+        public void Returns_ByDefault_WorksForHardCodedArgument()
+        {
+            IFileNameRules fakeRules = Substitute.For<IFileNameRules>();
+
+            fakeRules.IsValidLogFileName("strict.txt").Returns(true);
+
+            Assert.True(fakeRules.IsValidLogFileName("strict.txt"));
+
+        }
+    }
+}
