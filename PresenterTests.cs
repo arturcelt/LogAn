c374b6c2b9daa634d1677c2393b34ccb0aa6e12f
Artur Dybysławski <adybyslawski@hotmail.com>
2021-01-29 21:17:23 +0100

    REV_5.8

---
+++ PresenterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class PresenterTests
+    {
+        [Test]
+        public void ctor_WhenViewIsLoaded_CallsViewRender()
+        {
+            var mockView = Substitute.For<IView>();
+
+            Presenter p = new Presenter(mockView);
+            mockView.Loaded += Raise.Event<Action>();
+
+            mockView.Received().Render(Arg.Is<string>(s => s.Contains("Witaj świecie")));
+        }
+    }
+}
